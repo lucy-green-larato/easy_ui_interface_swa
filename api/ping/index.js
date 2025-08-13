e1d3ccc10153dfaa6a0f53df47602edd90f49779
Lucy Green <lucy.green@larato.co.uk>
2025-08-13 14:36:31 +0100

    Delete api/ping directory

--- api/ping/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req) {
-  context.res = {
-    status: 200,
-    headers: { "Content-Type": "text/plain", "Access-Control-Allow-Origin": "*" },
-    body: "pong"
-  };
-};
