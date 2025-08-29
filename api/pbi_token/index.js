4c982bbd5d323571910e3b1dddbfe3ca8983fd38
Lucy Green <lucy.green@larato.co.uk>
2025-08-29 10:48:58 +0000

    format errors fixed

--- api/pbi_token/index.js
+++ api/pbi_token/index.js
@@ -1,4 +1,3 @@
-// /api/pbi_token/index.js
 const msal = require('@azure/msal-node');
 const axios = require('axios');
 
@@ -6,61 +5,29 @@ module.exports = async function (context, req) {
   const { TENANT_ID, CLIENT_ID, CLIENT_SECRET, WORKSPACE_ID, REPORT_ID } = process.env;
 
   try {
-    // 0) AAD app token
+    // AAD app token
     const cca = new msal.ConfidentialClientApplication({
       auth: {
         clientId: CLIENT_ID,
         authority: `https://login.microsoftonline.com/${TENANT_ID}`,
-        clientSecret: CLIENT_SECRET,
-      },
+        clientSecret: CLIENT_SECRET
+      }
     });
 
     const { accessToken } = await cca.acquireTokenByClientCredential({
-      scopes: ['https://analysis.windows.net/powerbi/api/.default'],
+      scopes: ['https://analysis.windows.net/powerbi/api/.default']
     });
 
     const headers = { Authorization: `Bearer ${accessToken}` };
 
-    // (A) Get the report (for embedUrl + datasetId)
+    // 1) Get report to retrieve embedUrl (datasetId optional)
     const rep = await axios.get(
       `https://api.powerbi.com/v1.0/myorg/groups/${WORKSPACE_ID}/reports/${REPORT_ID}`,
       { headers }
     );
     const { embedUrl, datasetId } = rep.data;
 
-    // (B) --- SANITY CHECKS (step 3) ---
-    // Report users (look for your SP / group; accessRight should include 'Reshare')
-    const reportUsers = await axios.get(
-      `https://api.powerbi.com/v1.0/myorg/groups/${WORKSPACE_ID}/reports/${REPORT_ID}/users`,
-      { headers }
-    );
-
-    // Dataset users (look for 'Build' and 'Reshare')
-    const dsUsers = await axios.get(
-      `https://api.powerbi.com/v1.0/myorg/groups/${WORKSPACE_ID}/datasets/${datasetId}/users`,
-      { headers }
-    );
-
-    // Write to function logs
-    context.log('Report users:', JSON.stringify(reportUsers.data, null, 2));
-    context.log('Dataset users:', JSON.stringify(dsUsers.data, null, 2));
-
-    // If you want to see this in the browser, call /api/pbi-token?debug=1
-    if (req.query.debug === '1') {
-      return (context.res = {
-        status: 200,
-        headers: { 'Content-Type': 'application/json' },
-        body: {
-          debug: {
-            reportUsers: reportUsers.data,   // often under .value[]
-            datasetUsers: dsUsers.data,      // often under .value[]
-          },
-        },
-      });
-    }
-    // --- END SANITY CHECKS ---
-
-    // (C) Generate embed token
+    // 2) Generate an embed token for THIS report
     const tok = await axios.post(
       `https://api.powerbi.com/v1.0/myorg/groups/${WORKSPACE_ID}/reports/${REPORT_ID}/GenerateToken`,
       { accessLevel: 'View', allowSaveAs: false },
@@ -70,14 +37,14 @@ module.exports = async function (context, req) {
     context.res = {
       status: 200,
       headers: { 'Content-Type': 'application/json' },
-      body: { embedUrl, reportId: REPORT_ID, token: tok.data.token, datasetId },
+      body: { embedUrl, reportId: REPORT_ID, token: tok.data.token, datasetId }
     };
   } catch (err) {
     context.log.error(err.response?.data || err.message);
     context.res = {
       status: err.response?.status || 500,
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ message: err.message, data: err.response?.data }),
+      body: JSON.stringify({ message: err.message, data: err.response?.data })
     };
   }
 };
