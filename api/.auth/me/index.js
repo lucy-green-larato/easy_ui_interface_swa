5a73eba940b5347f5809d1002ea25939e63d2da7
Lucy Green <lucy.green@larato.co.uk>
2025-09-03 16:58:03 +0000

    local test fix

---
+++ api/.auth/me/index.js
@@ -0,0 +1,8 @@
+const body = {
+  clientPrincipal: {
+    identityProvider: "dev",
+    userId: "local-dev-user",
+    userDetails: "you@example.com",
+    userRoles: ["anonymous", "authenticated"]
+  }
+};
