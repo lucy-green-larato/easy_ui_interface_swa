23ff318e381c97f697b0e2454580c69689c6fa66
Lucy Green <lucy.green@larato.co.uk>
2025-09-06 13:33:25 +0000

    pass body through

--- api-python/CampaignOrchestration_HttpStart/__init__.py
+++ api-python/CampaignOrchestration_HttpStart/__init__.py
@@ -3,5 +3,9 @@ import azure.durable_functions as df
 
 async def main(req: func.HttpRequest, starter: str) -> func.HttpResponse:
     client = df.DurableOrchestrationClient(starter)
-    instance_id = await client.start_new("CampaignOrchestration", None, None)
+    try:
+        body = await req.get_json()
+    except Exception:
+        body = None
+    instance_id = await client.start_new("CampaignOrchestration", None, body)
     return client.create_check_status_response(req, instance_id)
